3fefe8fa7cc27c6e2c0bd2f94d9cbd5a6d4b2a40
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-15 11:12:17 +0530

    update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -18,9 +18,31 @@ Post request json file structure
 //to look the user schema look ../models/user.js
 
 const registerUser =async (req, res) => {
+    const { name, email, password } = req.body;
 
-    //Write you code here
-
+    try {
+      // Check if user with same email already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(404).json({ error: "User validation failed: email: Email already exists" });
+      }
+  
+      // Create new user with provided data
+      const newUser = new User({
+        name,
+        email,
+        password
+      });
+  
+      // Save the user to the database
+      const savedUser = await newUser.save();
+  
+      // Return the ObjectId of the newly created user
+      return res.status(200).json({ userId: savedUser._id });
+    } catch (error) {
+      // Return the error message if there's any error during registration
+      return res.status(404).json({ error: error.message });
+    }
 }
 
 module.exports = { registerUser };
